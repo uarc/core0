5bb08cb00b8b33a03440a00dda27c05850e0cc89
Geordon Worley <vadixidav@gmail.com>
2016-05-20 22:47:43 -0400

    add all opodes to ALU

--- src/alu.sv
+++ src/alu.sv
@@ -7,8 +7,19 @@ module alu(
   oc,
   oo,
 );
-  /// The width of the operands to the alu
-  parameter WIDTH = 32;
+  /// The magnitude of the width
+  parameter WIDTH_MAG = 5;
+  localparam WIDTH = 1 << WIDTH_MAG;
+
+  localparam
+    OP_LSL = 3'h0,
+    OP_LSR = 3'h1,
+    OP_CSL = 3'h2,
+    OP_CSR = 3'h3,
+    OP_ASR = 3'h4,
+    OP_AND = 3'h5,
+    OP_OR = 3'h6,
+    OP_ADD = 3'h7;
 
   /// The first parameter in a mathematical operation
   input [WIDTH-1:0] a;
@@ -17,18 +28,59 @@ module alu(
   /// Input carry bit
   input ic;
   /// Opcode which determines the operation
-  input [3:0] opcode;
+  input [2:0] opcode;
   /// Primary output of the operation
   output reg [WIDTH-1:0] out;
   /// Output carry bit
-  output oc;
+  output reg oc;
   /// Output overflow bit
-  output oo;
+  output reg oo;
+
+  wire [WIDTH:0] sum;
+
+  assign sum = a + b;
 
   always @* begin
     case (opcode)
-      // TODO: Add opcodes
-      default: out = 0;
+      OP_LSL: begin
+        out = a << b;
+        oc = 1'bx;
+        oo = 1'bx;
+      end
+      OP_LSR: begin
+        out = a >> b;
+        oc = 1'bx;
+        oo = 1'bx;
+      end
+      OP_CSL: begin
+        out = {a, a} >> (WIDTH - b[WIDTH_MAG-1:0]);
+        oc = 1'bx;
+        oo = 1'bx;
+      end
+      OP_CSR: begin
+        out = {a, a} >> b[WIDTH_MAG-1:0];
+        oc = 1'bx;
+        oo = 1'bx;
+      end
+      OP_ASR: begin
+        out = $signed(a) >>> b;
+        oc = 1'bx;
+        oo = 1'bx;
+      end
+      OP_AND: begin
+        out = a & b;
+        oc = 1'bx;
+        oo = 1'bx;
+      end
+      OP_OR: begin
+        out = a | b;
+        oc = 1'bx;
+        oo = 1'bx;
+      end
+      OP_ADD: begin
+        {oc, out} = sum;
+        oo = a[WIDTH-1] == b[WIDTH-1] && sum[WIDTH-1] != a[WIDTH-1];
+      end
     endcase
   end
 endmodule
