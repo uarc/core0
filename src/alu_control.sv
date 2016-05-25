e6e4225c61b61ea36610616fd02cd8965f207b81
Geordon Worley <vadixidav@gmail.com>
2016-05-24 20:30:57 -0400

    add initial alu_control module and all instructions in instructions.sv

---
+++ src/alu_control.sv
@@ -0,0 +1,36 @@
+`include "../src/instructions.sv"
+
+module alu_control(
+  instruction,
+  top,
+  second,
+  carry,
+
+  alu_a,
+  alu_b,
+  alu_ic,
+  alu_opcode,
+);
+  parameter WORD_WIDTH = 32;
+
+  input [7:0] instruction;
+  input [WORD_WIDTH-1:0] top;
+  input [WORD_WIDTH-1:0] second;
+  input carry;
+
+  output reg [WORD_WIDTH-1:0] alu_a;
+  output reg [WORD_WIDTH-1:0] alu_b;
+  output reg alu_ic;
+  output reg [2:0] alu_opcode;
+
+  always @* begin
+    case (instruction)
+      default: begin
+        alu_a = {WORD_WIDTH{1'bx}};
+        alu_b = {WORD_WIDTH{1'bx}};
+        alu_ic = 1'bx;
+        alu_opcode = 3'bx;
+      end
+    endcase
+  end
+endmodule
