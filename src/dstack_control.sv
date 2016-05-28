8ee6a36734830dca2447192d3daf331c6f1c21d7
Geordon Worley <vadixidav@gmail.com>
2016-05-27 22:29:01 -0400

    finish all instructions in alu_control

--- src/dstack_control.sv
+++ src/dstack_control.sv
@@ -2,28 +2,33 @@
 
 module dstack_control(
   instruction,
+  dcs,
+  dc_vals,
+  iterators,
   top,
   second,
   third,
   alu_out,
   mem_in,
   memload_last,
+  self_perimission,
+  self_address,
   receiver_self_permissions,
   receiver_self_addresses,
-  conveyor_access,
+  conveyor_value,
   movement,
   next_top,
 );
   parameter WORD_WIDTH = 32;
   parameter TOTAL_BUSES = 1;
-  parameter CONVEYOR_WIDTH = WORD_WIDTH;
 
   input [7:0] instruction;
+  input [3:0][WORD_WIDTH-1:0] dcs, dc_vals, iterators;
   input [WORD_WIDTH-1:0] top, second, third, alu_out, mem_in;
   input memload_last;
-  input [TOTAL_BUSES-1:0][WORD_WIDTH-1:0] receiver_self_permissions;
-  input [TOTAL_BUSES-1:0][WORD_WIDTH-1:0] receiver_self_addresses;
-  input [CONVEYOR_WIDTH-1:0] conveyor_access;
+  input [WORD_WIDTH-1:0] self_perimission, self_address;
+  input [TOTAL_BUSES-1:0][WORD_WIDTH-1:0] receiver_self_permissions, receiver_self_addresses;
+  input [WORD_WIDTH-1:0] conveyor_value;
   output reg [1:0] movement;
   output reg [WORD_WIDTH-1:0] next_top;
 
@@ -55,7 +60,57 @@ module dstack_control(
       `I_JNO: next_top = top;
       `I_JI: next_top = top;
       `I_JNI: next_top = top;
-      `I_CVZ: next_top = conveyor_access[WORD_WIDTH-1:0];
+      `I_CVZ: next_top = conveyor_value;
+      `I_READZ: next_top = dc_vals[instruction[1:0]];
+      `I_GETZ: next_top = dcs[instruction[1:0]];
+      `I_IZ: next_top = iterators[instruction[1:0]];
+      `I_P0: next_top = 0;
+      `I_DUP: next_top = top;
+      `I_GETP: next_top = self_perimission;
+      `I_GETA: next_top = self_address;
+      `I_WRITEZ: next_top = second;
+      `I_SETFZ: next_top = second;
+      `I_SETBZ: next_top = second;
+      `I_ADD: next_top = alu_out;
+      `I_ADDC: next_top = alu_out;
+      `I_SUB: next_top = alu_out;
+      `I_SUBC: next_top = alu_out;
+      `I_LSL: next_top = alu_out;
+      `I_LSR: next_top = alu_out;
+      `I_CSL: next_top = alu_out;
+      `I_CSR: next_top = alu_out;
+      `I_ASR: next_top = alu_out;
+      `I_AND: next_top = alu_out;
+      `I_OR: next_top = alu_out;
+      `I_XOR: next_top = alu_out;
+      `I_READA: next_top = second;
+      `I_CALL: next_top = second;
+      `I_JMP: next_top = second;
+      `I_ISET: next_top = second;
+      `I_SLB: next_top = second;
+      `I_USB: next_top = second;
+      `I_SEND: next_top = second;
+      `I_LOOPI: next_top = second;
+      `I_RWRITEZ: next_top = third;
+      `I_WRITE: next_top = third;
+      `I_JEQ: next_top = third;
+      `I_JNE: next_top = third;
+      `I_LES: next_top = third;
+      `I_LEQ: next_top = third;
+      `I_LESU: next_top = third;
+      `I_LEQU: next_top = third;
+      `I_IN: next_top = third;
+      `I_OUT: next_top = third;
+      `I_INCEPT: next_top = third;
+      `I_SET: next_top = third;
+      `I_SEL: next_top = third;
+      `I_SETA: next_top = third;
+      `I_LOOP: next_top = third;
+      `I_SEF: next_top = third;
+      `I_MUL: next_top = third;
+      `I_MULU: next_top = third;
+      `I_DIV: next_top = third;
+      `I_DIVU: next_top = third;
       default: next_top = {WORD_WIDTH{1'bx}};
     endcase
   end
