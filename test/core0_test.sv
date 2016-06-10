c40274fb4b44b269aedbd7be01c70dd7e5f8d88f
Geordon Worley <vadixidav@gmail.com>
2016-06-10 13:16:11 -0400

    add calli double nested test, no fixes necessary

--- test/core0_test.sv
+++ test/core0_test.sv
@@ -229,6 +229,19 @@ module core0_test;
     end
 
     $display("calli: %s", core0_base.core0.dstack_top == 2 ? "pass" : "fail");
+
+    $readmemh("bin/calli_double_nested_prog.list", programmem);
+    $readmemh("bin/calli_double_nested_data.list", mainmem);
+    programmem_read_value <= {MAIN_ADDR_WIDTH{1'bx}};
+    mainmem_read_value <= {MAIN_ADDR_WIDTH{1'bx}};
+    reset = 1;
+    clk = 0; #1; clk = 1; #1;
+    reset = 0;
+    for (int i = 0; i < 8; i++) begin
+      clk = 0; #1; clk = 1; #1;
+    end
+
+    $display("calli double nested: %s", core0_base.core0.dstack_top == 3 ? "pass" : "fail");
   end
 
   always @(posedge clk) begin
