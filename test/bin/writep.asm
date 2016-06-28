1055292f6559b9625528fa3515ab7910a05459f2
Geordon Worley <vadixidav@gmail.com>
2016-06-28 03:12:30 -0400

    add assembly source for the writep test

---
+++ test/bin/writep.asm
@@ -0,0 +1,39 @@
+# Add increment instruction to stack
+0x8
+# Add address of location to write instruction to
+1
+# Write instruction
+writep
+# Push 0 onto stack
+0
+# Blank push 0 instructions which will be overwritten
+0
+0
+0
+0
+0
+0
+0
+0
+0
+0
+0
+0
+0
+0
+0
+0
+0
+0
+0
+0
+0
+0
+0
+0
+0
+0
+0
+0
+# Fill out data space
+fill:0,2
