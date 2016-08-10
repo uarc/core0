a55a20e0680e9e3941710d383f71278a07e2953e
Geordon Worley <vadixidav@gmail.com>
2016-08-09 20:41:37 -0400

    double nested calls tested

--- test/core0_test.sv
+++ test/core0_test.sv
@@ -329,8 +329,7 @@ module core0_test;
 
     $display("calli: %s", core0_base.core0.dstack_top == 11 && core0_base.core0.dstack_second == 10 ? "pass" : "fail");
 
-    $readmemh("bin/calli_double_nested_prog.list", programmem);
-    $readmemh("bin/calli_double_nested_data.list", mainmem);
+    $readmemh("bin/calli_double_nested.list", programmem);
     receiver_sends = {TOTAL_BUSES{1'b0}};
     receiver_datas = {(TOTAL_BUSES * WORD_WIDTH){1'b0}};
     programmem_read_value <= {MAIN_ADDR_WIDTH{1'bx}};
@@ -338,11 +337,12 @@ module core0_test;
     reset = 1;
     clk = 0; #1; clk = 1; #1;
     reset = 0;
-    for (int i = 0; i < 8; i++) begin
+    for (int i = 0; i < 16; i++) begin
       clk = 0; #1; clk = 1; #1;
     end
 
-    $display("calli double nested: %s", core0_base.core0.dstack_top == 3 ? "pass" : "fail");
+    $display("calli double nested: %s",
+      core0_base.core0.dstack_top == 11 && core0_base.core0.dstack_second == 10 ? "pass" : "fail");
 
     $readmemh("bin/conditional_branching_prog.list", programmem);
     $readmemh("bin/conditional_branching_data.list", mainmem);
